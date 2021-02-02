9a7f6d4b0241436502790c6bed6da839779a1377
Piotr Marynczak <piotr.marynczak.programista@gmail.com>
2021-02-02 11:37:47 +0100

    added MVC folders and OkHTTP dependency

---
+++ WeatherApp/src/main/java/com/peter/weatherapp/WeatherApp/view/MainView.java
@@ -0,0 +1,16 @@
+package com.peter.weatherapp.WeatherApp.view;
+
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.spring.annotation.SpringUI;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.UI;
+
+@SpringUI(path = "")
+public class MainView extends UI {
+    @Override
+    protected void init(VaadinRequest vaadinRequest) {
+
+        
+
+    }
+}
