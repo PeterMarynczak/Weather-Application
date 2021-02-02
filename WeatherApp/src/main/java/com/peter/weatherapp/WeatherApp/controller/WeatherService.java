48b914926a624e823fcdfbe3578898ba66e07851
Piotr Marynczak <piotr.marynczak.programista@gmail.com>
2021-02-02 13:04:27 +0100

    added Service class to get remote information from weather API

---
+++ WeatherApp/src/main/java/com/peter/weatherapp/WeatherApp/controller/WeatherService.java
@@ -0,0 +1,34 @@
+package com.peter.weatherapp.WeatherApp.controller;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+
+@Service
+public class WeatherService {
+    private OkHttpClient client;
+    private Response response;
+
+    public JSONObject getWeather(String name) throws JSONException {
+
+        client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url("http://api.openweathermap.org/data/2.5/weather?q="+name+"&appid=36dee2f719fcadc7bbed52c0d7c39b75")
+                .build();
+
+        try {
+            response = client.newCall(request).execute();
+            return new JSONObject(response.body().string());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+
+    }
+}
--- WeatherApp/src/main/java/com/peter/weatherapp/WeatherApp/view/MainView.java
+++ WeatherApp/src/main/java/com/peter/weatherapp/WeatherApp/view/MainView.java
@@ -1,16 +1,29 @@
 package com.peter.weatherapp.WeatherApp.view;
 
+import com.peter.weatherapp.WeatherApp.controller.WeatherService;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.spring.annotation.SpringUI;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.UI;
+import org.json.JSONException;
+import org.springframework.beans.factory.annotation.Autowired;
+import javax.xml.ws.Service;
+
+import static javax.swing.UIManager.getString;
 
 @SpringUI(path = "")
 public class MainView extends UI {
+    @Autowired
+    private WeatherService weatherService;
     @Override
     protected void init(VaadinRequest vaadinRequest) {
 
-        
+        try {
+            System.out.println("Data: " + weatherService.getWeather("przemysl").getString("coord").toString());
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
 
     }
 }
