f193070097c794f1aeb87dfe1a76f2638fe057a3
Piotr Marynczak <piotr.marynczak.programista@gmail.com>
2021-02-02 11:06:28 +0100

    first commit

---
+++ WeatherApp/src/test/java/com/peter/weatherapp/WeatherApp/WeatherAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peter.weatherapp.WeatherApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
