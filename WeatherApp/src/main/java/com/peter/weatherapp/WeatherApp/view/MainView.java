fffd00f7bdf1f0543e1134e75e5578793d6de5f4
Piotr Marynczak <piotr.marynczak.programista@gmail.com>
2021-02-11 13:31:52 +0100

    added methods to set up Layout and Header

--- WeatherApp/src/main/java/com/peter/weatherapp/WeatherApp/view/MainView.java
+++ WeatherApp/src/main/java/com/peter/weatherapp/WeatherApp/view/MainView.java
@@ -3,8 +3,7 @@ package com.peter.weatherapp.WeatherApp.view;
 import com.peter.weatherapp.WeatherApp.controller.WeatherService;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.spring.annotation.SpringUI;
-import com.vaadin.ui.Label;
-import com.vaadin.ui.UI;
+import com.vaadin.ui.*;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -17,28 +16,67 @@ import static javax.swing.UIManager.getString;
 
 @SpringUI(path = "")
 public class MainView extends UI {
+
     @Autowired
     private WeatherService weatherService;
+    private VerticalLayout mainLayout;
+
     @Override
     protected void init(VaadinRequest vaadinRequest) {
+        setUpLayout();
+        setHeader();
+    }
 
-        try {
-            //System.out.println("Data: " + weatherService.getWeather("przemysl").getString("coord").toString());
-            JSONArray jsonArray = weatherService.returnWeatherArray("Przemysl");
-            JSONObject myObject = weatherService.returnMainObject("Przemysl");
+    private void setUpLayout() {
+        mainLayout = new VerticalLayout();
+        mainLayout.setWidth("100%");
+        mainLayout.setMargin(true);
+        mainLayout.setSpacing(true);
 
-            System.out.println("Id: " + myObject.getLong("pressure"));
+        mainLayout.setDefaultComponentAlignment(Alignment.MIDDLE_CENTER);
 
-            for (int i = 0; i < jsonArray.length() ; i++) {
-                JSONObject weatherObject = jsonArray.getJSONObject(i);
-
-                System.out.println("Id: " + weatherObject.getInt("id") + ":, main: " + weatherObject.getString("main") +
-                    ", description: " + weatherObject.getString("description"));
-            }
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-
+        setContent(mainLayout);
 
     }
+
+    private void setHeader() {
+        HorizontalLayout headerLayout = new HorizontalLayout();
+        headerLayout.setDefaultComponentAlignment(Alignment.MIDDLE_CENTER);
+
+        Label title = new Label("Weather!");
+
+        headerLayout.addComponents(title);
+
+        mainLayout.addComponents(headerLayout);
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//        try {
+//            //System.out.println("Data: " + weatherService.getWeather("przemysl").getString("coord").toString());
+//            JSONArray jsonArray = weatherService.returnWeatherArray("Przemysl");
+//            JSONObject myObject = weatherService.returnMainObject("Przemysl");
+//
+//            System.out.println("Id: " + myObject.getLong("pressure"));
+//
+//            for (int i = 0; i < jsonArray.length() ; i++) {
+//                JSONObject weatherObject = jsonArray.getJSONObject(i);
+//
+//                System.out.println("Id: " + weatherObject.getInt("id") + ":, main: " + weatherObject.getString("main") +
+//                    ", description: " + weatherObject.getString("description"));
+//            }
+//        } catch (JSONException e) {
+//            e.printStackTrace();
+//        }
